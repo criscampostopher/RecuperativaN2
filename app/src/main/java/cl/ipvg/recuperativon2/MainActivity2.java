464064f51da7d1dfcd54857304c6b51dbf7ca203
crisc <cristopher.campos2@virginiogomez.cl>
2024-11-04 23:28:18 -0300

    Se agrego el pasar de pantalla en pantalla, se agrego el mapa(Falta la key de la api).

--- app/src/main/java/cl/ipvg/recuperativon2/MainActivity2.java
+++ app/src/main/java/cl/ipvg/recuperativon2/MainActivity2.java
@@ -1,6 +1,10 @@
 package cl.ipvg.recuperativon2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button btMostrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
+        btMostrar=(Button) findViewById(R.id.btMostrar);
+        Intent intent= new Intent(MainActivity2.this, MapsActivity.class);
+
+        btMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
